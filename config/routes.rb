055d6b82405b59cefe5e1dc49b4866bbd4872d80
Evgeniy Kuznetsov <chamscontacts@gmail.com>
2015-02-07 23:49:19 +0300

    Закоментил создание

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Sprintimer::Application.routes.draw do
 
   root to: "timer#index"
 
-  scope format: true, constraints: { format: 'json' } do
-    resources :timer, only: [:create]
-  end
+  #scope format: true, constraints: { format: 'json' } do
+  #  resources :timer, only: [:create]
+  #end
 
 end
--- spec/controllers/timer_controller_spec.rb
+++ spec/controllers/timer_controller_spec.rb
@@ -6,12 +6,16 @@ describe TimerController do
   context "Create timer" do
 
     it "If not pass data when response unsuccessful" do
+      pending
+
       post "create", format: :json
       response.should_not be_success
       Timer.all.size.should==0
     end
 
     it "Creates timer" do
+      pending
+
       post "create", format: :json, data: "{1: 1000}"
       response.should be_success
       Timer.all.size.should==1
