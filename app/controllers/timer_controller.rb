00d39113761f746c0499a55a2265556318c7c22d
Evgeniy Kuznetsov <chamscontacts@gmail.com>
2015-02-07 23:37:07 +0300

    Создание таймеров

--- app/controllers/timer_controller.rb
+++ app/controllers/timer_controller.rb
@@ -5,6 +5,14 @@ class TimerController < ApplicationController
   end
 
   def create
+    data = params[:data]
+    @timer = Timer.create data: data
+
+    if @timer.errors.size>0
+      render status: 422, template: "timer/create_error"
+    else
+      render status: 201
+    end
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Sprintimer::Application.routes.draw do
 
   root to: "timer#index"
 
-  resources :timer, only: [:create]
+  scope format: true, constraints: { format: 'json' } do
+    resources :timer, only: [:create]
+  end
 
 end
