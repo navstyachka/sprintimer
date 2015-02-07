3c97f7b4b0d347c25eeb279f206e90ccb20fd609
Evgeniy Kuznetsov <chamscontacts@gmail.com>
2015-02-07 21:33:34 +0300

    Added rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sprintimer::Application.config.secret_key_base = 'e4e7f455c66a42306ea98b3bcd0d37a453227704a3f96bc9acd7dfa119726af17368e57f0081d1b0a283b69010b837d279de20326028dea7f921f39831ffff4c'
